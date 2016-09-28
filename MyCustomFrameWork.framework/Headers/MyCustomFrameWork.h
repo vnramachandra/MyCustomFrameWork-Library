ed310f47c872c82061871fe9c85c3dfc8f90cfc1
vnramachandra <ramachandra.naragund@robosoftin.com>
2016-09-28 18:18:26 +0530

    Added v1.0.0

---
+++ MyCustomFrameWork.framework/Headers/MyCustomFrameWork.h
@@ -0,0 +1,20 @@
+//
+//  MyCustomFrameWork.h
+//  MyCustomFrameWork
+//
+//  Created by Ramachandra Naragund on 28/09/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCustomFrameWork.
+FOUNDATION_EXPORT double MyCustomFrameWorkVersionNumber;
+
+//! Project version string for MyCustomFrameWork.
+FOUNDATION_EXPORT const unsigned char MyCustomFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCustomFrameWork/PublicHeader.h>
+
+
+#import "MyMainClass.h"
